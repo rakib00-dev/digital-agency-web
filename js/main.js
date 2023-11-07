be2e43a78ed379e246f7546ed47256da9836286e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gamil.com>
2023-11-07 09:21:18 +0600

    fixed nav bar on scroll effect 2nd time

--- js/main.js
+++ js/main.js
@@ -4,17 +4,15 @@ let navBarLink = document.querySelectorAll('.scroll-link');
 window.addEventListener('scroll', () => {
   const scrolling = window.scrollY;
   const numPlus = 44;
-  const numNo = 28;
+  const numMinus = 28;
 
   if (scrolling >= numPlus) {
     navBar.classList.add('navBarScrolling');
     navBarLink.classList.add('scroll-a');
     navBarLink.style.color = 'red';
     console.log('fix');
-  } else if (scrolling <= numNo) {
+  } else if (scrolling <= numMinus) {
     navBar.classList.remove('navBarScrolling');
     console.log('unfixed');
   }
-
-  // console.log(scrolling);
 });
