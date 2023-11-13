398cbc7ccba29b2c363c68da4cc2826a3b0ab85c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gamil.com>
2023-11-13 09:33:20 +0600

    fixed the nav

--- js/main.js
+++ js/main.js
@@ -1,47 +1,27 @@
 let navBar = document.querySelector('nav');
 let navBarLink = document.querySelectorAll('.scroll-link');
-let logo = document.querySelector('#nav-logo');
+let logo = document.querySelector('#logo-scroll');
+let navButton = document.querySelector('#nav-button-scroll');
 
 window.addEventListener('scroll', () => {
   const scrolling = window.scrollY;
-  // const numPlus = 44;
   const numPlus = 44;
   const numMinus = 28;
 
   if (scrolling >= numPlus) {
     navBar.classList.add('navBarScrolling');
 
+    logo.style.color = '#6222cc';
     navBarLink.forEach((link) => {
       link.style.color = 'black';
     });
-    logo.styel.color = 'blue';
-    console.log('fix');
+    navButton.style.background = '#fba504';
   } else if (scrolling <= numMinus) {
+    navBar.classList.remove('navBarScrolling');
+    logo.style.color = null;
     navBarLink.forEach((link) => {
       link.style.color = '';
     });
-    navBar.classList.remove('navBarScrolling');
-    console.log('unfixed');
+    navButton.style.background = null;
   }
 });
-
-// const navBar = document.querySelector('nav');
-// const navBarLinks = document.querySelectorAll('.scroll-link');
-
-// window.addEventListener('scroll', () => {
-//   const scrolling = window.scrollY;
-
-//   if (scrolling > 0) {
-//     navBar.classList.add('navBarScrolling');
-//     navBarLinks.forEach(link => {
-//       link.classList.add('scroll-a');
-//       link.style.color = 'red';
-//     });
-//   } else if (scrolling === 0) {
-//     navBar.classList.remove('navBarScrolling');
-//     navBarLinks.forEach(link => {
-//       link.classList.remove('scroll-a');
-//       link.style.color = '';
-//     });
-//   }
-// });
